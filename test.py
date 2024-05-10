025741a5b1b10609686249490835f388b4450f5b
re23m023 <re23m023@technikum-wien.at>
2024-05-10 17:20:19 +0200

    Aktualisiere test.py mit KNN-Beispiel

--- test.py
+++ test.py
@@ -1,36 +1,23 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report
 import numpy as np
-from sklearn.metrics import accuracy_score
-from sklearn.utils import shuffle
-from knn_classifier import train_knn, evaluate_knn
-from feature_extraction import apply_pca, apply_tsne
-from tensorflow.keras.datasets import cifar10
-from statsmodels.stats.contingency_tables import mcnemar
 
-def mcnemars_test(y_true, pred1, pred2):
-    """
-    Perform McNemar's test on two sets of predictions.
-    """
-    table = np.zeros((2, 2), dtype=int)
-    for a, b in zip(pred1, pred2):
-        table[int(a)][int(b)] += 1
-    result = mcnemar(table, exact=True)
-    return result
-
+# Beispiel: CIFAR-10 Daten laden
 def load_and_preprocess_cifar10():
-    """
-    Load and preprocess the CIFAR-10 dataset.
-    """
+    from keras.datasets import cifar10
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x_train, y_train = shuffle(x_train, y_train, random_state=42)
-    x_train = x_train[:10000].reshape(10000, -1) / 255.0
-    y_train = y_train[:10000].ravel()
-    x_test = x_test.reshape(x_test.shape[0], -1) / 255.0
+
+    # Einfaches Preprocessing (Normierung)
+    x_train = x_train.astype('float32') / 255.0
+    x_test = x_test.astype('float32') / 255.0
+
+    # Daten reshapen und Labels konvertieren
+    x_train = x_train.reshape((x_train.shape[0], -1))
+    x_test = x_test.reshape((x_test.shape[0], -1))
+
+    y_train = y_train.ravel()
     y_test = y_test.ravel()
+
     return x_train, y_train, x_test, y_test
 
-def main():
-    """
-    Main function to run KNN classification with PCA and t-SNE.
-    """
-    # Load CIFAR-10 data
-   
+x_train, y_train, x_test, y_test = load_and_preprocess_cifar10()
