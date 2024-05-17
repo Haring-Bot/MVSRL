0955ce807d18ebf0cb8b81984689194a5eb66036
re23m023 <re23m023@technikum-wien.at>
2024-05-17 18:05:08 +0200

    now working

--- KNN.py
+++ KNN.py
@@ -3,19 +3,25 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-from keras.datasets import cifar10
+from sklearn.datasets import fetch_openml
+#hier decomposition und mcnemar importieren
 
 # CIFAR-10-Datenset laden
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+cifar10 = fetch_openml('CIFAR_10', version=1)
+
+# Daten und Labels extrahieren
+X = cifar10.data
+y = cifar10.target.astype(np.int64)  # Labels müssen in Integer umgewandelt werden
 
 # Daten vorverarbeiten
 # Die Bilder haben die Dimensionen (32, 32, 3), also flachen wir sie zu Vektoren der Länge 3072 ab
-x_train = x_train.reshape((x_train.shape[0], -1))
-x_test = x_test.reshape((x_test.shape[0], -1))
+X = X.reshape((X.shape[0], -1))
 
 # Normalisierung der Daten
-x_train = x_train.astype('float32') / 255.0
-x_test = x_test.astype('float32') / 255.0
+X= X.astype('float32') / 255.0
+
+# Aufteilen der Daten in Trainings- und Testsets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #Hier kommt Merkmalsextraktion hin
 
@@ -30,10 +36,10 @@ y_pred = knn.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Genauigkeit: {accuracy*100:.2f}%")
 
-# KNN mit PCA
-knn_pca = KNeighborsClassifier(n_neighbors=3)
-knn_pca.fit(x_train_pca, y_train.ravel())
-y_pred_pca = knn_pca.predict(x_test_pca)
-accuracy_pca = accuracy_score(y_test, y_pred_pca)
-print(f"Genauigkeit mit PCA: {accuracy_pca*100:.2f}%")
+# KNN mit Merkmalextrahierer A
+knn_A = KNeighborsClassifier(n_neighbors=3)
+knn_A.fit(x_train_A, y_train.ravel())
+y_pred_A = knn_A.predict(x_test_A)
+accuracy_A = accuracy_score(y_test, y_pred_A)
+print(f"Genauigkeit mit PCA: {accuracy_A*100:.2f}%")
 
