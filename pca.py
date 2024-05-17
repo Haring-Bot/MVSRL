d67b653233fa771fafbb2dc2a58d36dcea2d5c48
AlbertL98 <lorant._albert@hotmail.com>
2024-05-17 18:37:29 +0200

    Pca V2

--- pca.py
+++ pca.py
@@ -3,9 +3,33 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-NUM_DIMENSIONS = 500
+NUM_DIMENSIONS = 100
 NUM_TRAIN_DATA = 1000
 NUM_TEST_DATA = 2000
+
+
+def get_pca_features(imageData, dimensions, print_pca_data=False):
+    pca = PCA(n_components=dimensions)
+    fData = pca.fit_transform(imageData)
+
+    # DEBUGING
+    if print_pca_data:
+        # print variance per dimension
+        print('Explained variance by each component: ', pca.explained_variance_ratio_)
+
+        # plot variance over components
+        plt.figure(figsize=(8, 6))
+        plt.plot(np.cumsum(pca.explained_variance_ratio_))
+        plt.xlabel('Number of Components')
+        plt.ylabel('Cumulative Explained Variance')
+        plt.show()
+
+        # print accuracy by reconstruct image from pca
+        data_inverse = pca.inverse_transform(fData)
+        reconstruction_error = np.mean((imageData - data_inverse) ** 2)
+        print('Reconstruction error: ', reconstruction_error)
+
+    return fData
 
 def main():
     # Load CIFAR-10 data
@@ -27,30 +51,12 @@ def main():
     X_test = X_test.reshape((len(X_test), -1))
 
     # Apply PCA
-    pca = PCA(n_components=NUM_DIMENSIONS)
-    X_train_pca = pca.fit_transform(X_train)
-    X_test_pca = pca.transform(X_test)
+    X_train_pca = get_pca_features(X_train, NUM_DIMENSIONS, False)
+    X_test_pca = get_pca_features(X_test, NUM_DIMENSIONS)
 
     # Save the PCA transformed data
     np.save('X_train_pca.npy', X_train_pca)
     np.save('X_test_pca.npy', X_test_pca)
-
-    #print variance per dimension
-    print('Explained variance by each component: ', pca.explained_variance_ratio_)
-
-
-    #plot variance over components
-    plt.figure(figsize=(8, 6))
-    plt.plot(np.cumsum(pca.explained_variance_ratio_))
-    plt.xlabel('Number of Components')
-    plt.ylabel('Cumulative Explained Variance')
-    plt.show()
-
-    #print accuracy by reconstruct image from pca
-    X_train_inverse = pca.inverse_transform(X_train_pca)
-    reconstruction_error = np.mean((X_train - X_train_inverse) ** 2)
-    print('Reconstruction error: ', reconstruction_error)
-
 
 # set name if run directly
 if __name__ == "__main__":
