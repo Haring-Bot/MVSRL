e1b15e7f38aa0c8943d6950ca12aed23828e414c
JulianH <haringjulian@gmail.com>
2024-05-17 18:53:28 +0200

    Dataset  is now downloaded automatically. Code is now also more modular

--- readData.py
+++ readData.py
@@ -9,28 +9,32 @@
 import tarfile
 import pickle
 import numpy as np
+from urllib.request import urlretrieve
+import os
 
 NUM_IMAGE_TRAIN = 10000
 NUM_IMAGE_TEST = 2000
 
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
 def load_cifar10_data(path_to_dataset):
     # Unpickle function
-    def unpickle(file):
-        with open(file, 'rb') as fo:
-            dict = pickle.load(fo, encoding='bytes')
-        return dict
+    pathToBatches = "dataset/cifar-10-batches-py/"
 
     tar = tarfile.open(path_to_dataset, "r:gz")
-    tar.extractall()
+    tar.extractall(path = "dataset/")
     tar.close()
 
     train_data = []
     train_labels = []
 
+    print("trying to load the dataset")
     # The CIFAR-10 dataset consists of 5 batches, named data_batch_1, data_batch_2, etc..
     for i in range(1, 6):
-        data_dict = unpickle("cifar-10-batches-py/data_batch_" + str(i))
+        data_dict = unpickle(pathToBatches+ "data_batch_" + str(i))
         if i == 1:
             train_data = data_dict[b'data']
             train_labels = data_dict[b'labels']
@@ -38,7 +42,7 @@ def load_cifar10_data(path_to_dataset):
             train_data = np.vstack((train_data, data_dict[b'data']))
             train_labels = np.hstack((train_labels, data_dict[b'labels']))
 
-    test_data_dict = unpickle("cifar-10-batches-py/test_batch")
+    test_data_dict = unpickle(pathToBatches + "test_batch")
     test_data = test_data_dict[b'data']
     test_labels = np.array(test_data_dict[b'labels'])
 
@@ -52,10 +56,30 @@ def load_cifar10_data(path_to_dataset):
 
     return train_data_subset, train_labels_subset, test_data_subset, test_labels_subset
 
+def downloadDataset(url):
+    filename = "downloads/CIFAR-10_pickled.tar.gz"
+
+    if os.path.exists(filename):
+        print("found data locally")
+        return filename
+
+    try:
+        print("downloading data from:", url)
+        urlretrieve(url, filename)
+        print("download complete")
+        return filename
+    except Exception as e:
+        print(f"An error occurred while downloading: {e}")
+        return None  # Return None or raise the exception based on your needs
+
+
+
 
 def main():
-    path_to_dataset = "cifar-10-python.tar.gz"  # replace with your path
-    train_data, train_labels, test_data, test_labels = load_cifar10_data(path_to_dataset)
+    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+
+    path = downloadDataset(url)
+    train_data, train_labels, test_data, test_labels = load_cifar10_data(path)
 
     # Display the first image
     img = train_data[0].reshape((3, 32, 32)).transpose((1, 2, 0))
