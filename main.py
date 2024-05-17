4a34c23e76606b02fa84aa1f6000725cd67300e7
JulianH <haringjulian@gmail.com>
2024-05-17 19:37:22 +0200

    added notes to main

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+#notes:
+#   müssen wir die Daten standardisieren?
+#   zahlt sich scikit_learn pipeline aus
+#   SVM statt KNN?
+
+# ToDOs:
+#   Daten standardisieren
+#   scikit_learn pipeline?
+#   K nearest neighbour implementieren
+#   Daten auswerten
+
+
+
+
 import readData
 
 readData.main()
