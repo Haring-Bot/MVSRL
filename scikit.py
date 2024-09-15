f7fe2a7da4bcd6e615d4d73192ba60259db8c62a
AlbertL98 <lorant._albert@hotmail.com>
2024-09-15 11:26:14 +0200

    Add files via upload

--- scikit.py
+++ scikit.py
@@ -7,6 +7,9 @@ from sklearn.base import BaseEstimator
 from sklearn.base import TransformerMixin
 from skimage import color
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 
@@ -42,10 +45,24 @@ class HOGTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         # Reshape and apply HOG to each image in the dataset
-        return [hog(color.rgb2gray(img.reshape(self.image_shape)), 
+        return [hog(img,
                     pixels_per_cell=self.pixels_per_cell,
                     cells_per_block=self.cells_per_block,
-                    orientations=self.orientations) for img in X]
+                    orientations=self.orientations,
+                    channel_axis=2) for img in X]
+
+
+def reshape_cifar_image(image):
+    """
+    Reshape a flattened CIFAR-10 image to its original format.
+    """
+    # Separate the color channels
+    red = image[:1024].reshape(32, 32)
+    green = image[1024:2048].reshape(32, 32)
+    blue = image[2048:].reshape(32, 32)
+
+    # Stack the color channels to create a 32x32x3 image
+    return np.dstack((red, green, blue))
 
 
 def main():
@@ -56,6 +73,18 @@ def main():
     testData = np.load("dataset/dataset_split/testData.npy")
     trainLabel = np.load("dataset/dataset_split/trainLabel.npy")
     testLabel = np.load("dataset/dataset_split/testLabel.npy")
+
+    trainData = np.array([reshape_cifar_image(image) for image in trainData])
+    testData = np.array([reshape_cifar_image(image) for image in testData])
+
+    # Test show one image
+    #plt.imshow(trainData[0])
+    #plt.show()
+
+    print(f"X_train shape: {trainData.shape}")
+    print(f"y_train shape: {trainLabel.shape}")
+    print(f"X_test shape: {testData.shape}")
+    print(f"y_test shape: {testLabel.shape}")
 
     bestAccuracy = 0 
     bestPCAdim = 0
@@ -76,28 +105,28 @@ def main():
     ])
 
     #GridSearch for parameter tuning
-    gridSearchPCA = GridSearchCV(pipelinePCA, PCAgrid, cv = 5, scoring = "accuracy")
+    #gridSearchPCA = GridSearchCV(pipelinePCA, PCAgrid, cv = 5, scoring = "accuracy")
     gridSearchHOG = GridSearchCV(pipelineHOG, HOGgrid, cv=5, scoring = "accuracy")
 
     #train pipelines
-    print("start training...")
-    gridSearchPCA.fit(trainData, trainLabel)
+    #print("start training...")
+    #gridSearchPCA.fit(trainData, trainLabel)
     gridSearchHOG.fit(trainData, trainLabel)
 
     #find best model
-    bestModelPCA = gridSearchPCA.best_estimator_
+    #bestModelPCA = gridSearchPCA.best_estimator_
     bestModelHOG = gridSearchHOG.best_estimator_
     
     #predict
-    testPredictionsPCA = bestModelPCA.predict(testData)
+    #testPredictionsPCA = bestModelPCA.predict(testData)
     testPredictionsHOG = bestModelHOG.predict(testData)
     
     #accuracy
-    accuracyPCA = accuracy_score(testLabel, testPredictionsPCA)
+    #accuracyPCA = accuracy_score(testLabel, testPredictionsPCA)
     accuracyHOG = accuracy_score(testLabel, testPredictionsHOG)
     
     #results
-    print(f"\n Training completed! \nAn accuracy of {accuracyPCA:.2f} can be achieved with ", gridSearchPCA.best_params_)
+    #print(f"\n Training completed! \nAn accuracy of {accuracyPCA:.2f} can be achieved with ", gridSearchPCA.best_params_)
     print(f"\n Training completed! \nAn accuracy of {accuracyHOG:.2f} can be achieved with ", gridSearchHOG.best_params_)
 
 
@@ -117,6 +146,7 @@ def main():
     #print(f"PCA = {PCAdimensions:d}, KNNneighbors = {KNNneighbors:d}  Accuracy = {accuracyPCA:.2f}")
     #print(f"The highest Accuracy of {bestAccuracy:.2f} was achieved with reducing dimensions via PCA to {bestPCAdim:d} and a KNN neighbors value of {bestKNNneighbors:d}")
 
+    print("End Code")
 
 if __name__ == "__main__":
     main()
